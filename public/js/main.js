6a73a62507f386537ff4783d007fc83588d42822
Connor-Kubas <conman1507@gmail.com>
2022-10-03 02:24:24 -0500

    WIP

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+dragElement(document.getElementById(['mountain']));
+
+    function dragElement(card) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        
+        card.onmousedown = dragMouseDown;
+        
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            card.style.top = (card.offsetTop - pos2) + "px";
+            card.style.left = (card.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            /* stop moving when mouse button is released:*/
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+}
